ac5407f9da1d6204f3cbdd3eb0d23dc23d1c6844
Jason Bagwill <mr.bagwill@gmail.com>
2022-05-04 21:08:42 -0400

    script additions

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ last_name = "Blevins"
 def account_generator(first_name, last_name):
   return first_name[0:3] + last_name[0:3]
 
+
 new_account = account_generator("Julie", "Blevins")
 print(new_account)
 company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
@@ -10,3 +11,8 @@ second_to_last = company_motto[-2]
 print(second_to_last)
 final_word = company_motto[-4:]
 print(final_word)
+
+#fixing Bob to Rob
+first_name = "Bob"
+last_name = "Daily"
+fixed_first_name = "R" + first_name[-2:]
