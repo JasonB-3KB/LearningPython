aef05cdfa0d7cd6ef5946978262331b07fb65971
Jason Bagwill <mr.bagwill@gmail.com>
2022-05-04 21:01:57 -0400

    added to script.py

--- script.py
+++ script.py
@@ -5,3 +5,8 @@ def account_generator(first_name, last_name):
 
 new_account = account_generator("Julie", "Blevins")
 print(new_account)
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+second_to_last = company_motto[-2]
+print(second_to_last)
+final_word = company_motto[-4:]
+print(final_word)
