1c73245aed140208aa4da077249c82291fe34f34
Jason Bagwill <mr.bagwill@gmail.com>
2022-05-05 20:57:09 -0400

    added username generator to script

--- script.py
+++ script.py
@@ -16,3 +16,23 @@ print(final_word)
 first_name = "Bob"
 last_name = "Daily"
 fixed_first_name = "R" + first_name[-2:]
+
+#creating a username and password generator
+def username_generator(first_name, last_name):
+    if len(first_name) < 3:
+        user_name = first_name
+    else:
+        user_name = first_name[0:3]
+    if len(last_name) < 4:
+        user_name += last_name
+    else:
+        user_name += last_name[0:4]
+    return user_name
+  
+    
+def password_generator(user_name):
+    password = ""
+    for i in range(0, len(user_name)):
+        password += user_name[i-1]
+    return password
+
