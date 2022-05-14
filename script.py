a79ab268c6dd574e3a89fb3f94f84d32eb71e723
Jason Bagwill <mr.bagwill@gmail.com>
2022-05-13 20:58:58 -0400

    added the "format" function

--- script.py
+++ script.py
@@ -136,3 +136,19 @@ print(toomer_bio_fixed)
 #find function
 find_test = "This is a test phrase to find the word/'s index"
 print(find_test.find('phrase'))
+
+
+
+#Using a specific "format" function
+def poem_description(publishing_date, author, title, original_work):
+  poem_desc = "The poem {title} by {author} was originally published in {original_work} in {publishing_date}.".format(publishing_date = publishing_date, author = author, title = title, original_work = original_work)
+  return poem_desc
+
+author = "Shel Silverstein"
+title = "My Beard"
+original_work = "Where the Sidewalk Ends"
+publishing_date = "1974"
+
+my_beard_description = poem_description(publishing_date, author, title, original_work)
+
+print(my_beard_description)
